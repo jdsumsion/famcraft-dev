0d13b0476bbc841e80eb4c38bf548fd913fc82d5
John Sumsion <sumsionjg@familysearch.org>
2018-05-30 22:48:11 -0600

    Create different event handlers to support 3 game modes

--- plugin/src/main/java/org/familysearch/famcraft/FamCraftGameMode.java
+++ plugin/src/main/java/org/familysearch/famcraft/FamCraftGameMode.java
@@ -7,24 +7,46 @@ import org.bukkit.entity.Player;
 import org.bukkit.material.Door;
 
 /**
- * Responsible for allowing player to switch between game modes.
+ * Allows game modes to be implemented without having all the code mixed together in the root plugin.
  *
  * @author John Sumsion
  */
 public interface FamCraftGameMode {
 
-  default void onItemCrouch(Player player, Block block) {
+  // LOCATION: allows clue locations to be identified (double-crouch for picture walls)
+  // HUNT: allows hidden clues to be uncovered
+  default void onBlockCrouch(Player player, Block block) {
   }
 
+  // ANCESTOR: allows pictures, stories, events to be identified when setting up parameters for a given ancestor
+  default void onBlockBreak(Player player, Block block) {
+  }
+
+  // ANCESTOR: allows pictures, stories, events to be identified when setting up parameters for a given ancestor
+  default void onPickupItemSelection(Player player, Material material) {
+  }
+
+  // LOCATION: allows boundary door locations to be identified (for notifying about clues inside)
   default void onDoorCrouch(Player player, Block block, Door door) {
   }
 
-  default void onChestCrouch(Player player, Block block, Chest chest) {
-  }
-
+  // HUNT: allows clue counts to be displayed that are guarded by this door
   default void onDoorOpen(Player player, Block block, Door door) {
   }
 
-  default void onInventoryItemPlace(Player player, Material material) {
+  // LOCATION: allows clue locations to be tied to a door (those clues that were selected between the door crouch and the close)
+  default void onDoorClose(Player player, Block block, Door door) {
+  }
+
+  // HUNT: allows for sounds to be played if there is a clue inside a chest
+  default void onChestOpen(Player player, Chest chest) {
+  }
+
+  // HUNT: allows fact/story clues to be presented to the user as special records are discovered in chests
+  default void onInventoryItemSelection(Player player, Material material) {
+  }
+
+  // HUNT: allows fact/story clues to be presented to the user as special records are discovered in chests
+  default void onDiscPlay(Player player, Material material) {
   }
 }
---
+++ plugin/src/main/java/org/familysearch/famcraft/setup/AncestorHuntMode.java
@@ -0,0 +1,11 @@
+package org.familysearch.famcraft.setup;
+
+import org.familysearch.famcraft.FamCraftGameMode;
+
+/**
+ * Responsible for implementing a random scavenger hunt given a world that is set up and a selected ancestor.
+ *
+ * @author John Sumsion
+ */
+public class AncestorHuntMode implements FamCraftGameMode {
+}
